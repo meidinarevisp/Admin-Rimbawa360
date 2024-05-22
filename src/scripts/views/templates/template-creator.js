43b3a1b248f3b95daeca4cc99d971d1bcd720be1
meidinarevisp <revimeidina72@gmail.com>
2024-05-22 20:50:18 +0700

    update

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,5 +1,21 @@
 const dashboardTemplate = () => `
-  
+   <section class="dashboard-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1>Selamat Datang di Dashboard!</h1>
+        </div>
+        <div class="col-12 text-center mt-4">
+          <img src="rimbawa-360.png" alt="Rimbawa 360" style="max-width: 500px;">
+        </div>
+        <div class="col-12 text-center mt-4">
+          <a href="#" class="btn btn-dashboard btn-lg me-3">Ekowisata</a>
+          <a href="#" class="btn btn-dashboard btn-lg me-3">Spesies</a>
+          <a href="#" class="btn btn-dashboard btn-lg">Edukasi</a>
+        </div>
+      </div>
+    </div>
+  </section>
 `;
 const direktoriTemplate = () => `
   <div class="dropdown d-inline">
