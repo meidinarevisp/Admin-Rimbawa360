1c9cdb89fb637ddc05a0c4619170fc6885972fb9
meidinarevisp <revimeidina72@gmail.com>
2024-05-25 11:56:49 +0700

    update

--- src/scripts/views/pages/edukasi.js
+++ src/scripts/views/pages/edukasi.js
@@ -1,5 +1,6 @@
 import UrlParser from "../../routes/url-parser";
 import { edukasiTemplate } from "../templates/template-creator";
+import edukasiData from "../../../data/Edukasi.json";
 
 const Edukasi = {
   async render() {
@@ -9,7 +10,37 @@ const Edukasi = {
     return renderedTemplate;
   },
 
-  async afterRender() {},
+  async afterRender() {
+    const edukasiContainer = document.querySelector("#edukasi");
+    const data = edukasiData.edukasi_dan_kesadaran_lingkungan_hutan;
+
+    data.forEach((item) => {
+      const truncatedDeskripsi =
+        item.deskripsi.length > 150
+          ? item.deskripsi.substring(0, 100) + "..."
+          : item.deskripsi;
+      const itemElement = document.createElement("div");
+      itemElement.classList.add("card", "mb-3");
+      itemElement.innerHTML = `
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${item.gambar}" class="img-fluid rounded-start" alt="${item.nama_tempat}">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${item.nama_isu}</h5>
+                            <p class="card-text">${truncatedDeskripsi}</p>
+                            <div class="d-flex justify-content-end">
+                                <button class="btn btn-edit rounded-circle me-2"><i class="fas fa-edit"></i></button>
+                                <button class="btn btn-hapus rounded-circle"><i class="fas fa-trash-alt"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+      edukasiContainer.appendChild(itemElement);
+    });
+  },
 };
 
 export default Edukasi;
